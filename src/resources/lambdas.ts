3022f41114c0e4003e628305a6daca9222bf9ec5
aparrett <anthonyparrett7@gmail.com>
2024-04-04 11:40:33 -0500

    keep lambda warm

--- src/resources/lambdas.ts
+++ src/resources/lambdas.ts
@@ -4,10 +4,17 @@ export default {
     timeout: 29,
     events: [
       {
+        // keep the lambda warm
+        eventBridge: {
+          // every 5 minutes between 7AM and 11PM
+          schedule: 'cron(0/5 12-24 * * ? *)',
+        },
+      },
+      {
         http: {
           method: 'ANY',
           path: '/',
-          cors: true
+          cors: true,
         },
       },
       {
