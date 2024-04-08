fa41c8cd116eb31f384ae7f841f99e60c7bf55ab
aparrett <anthonyparrett7@gmail.com>
2024-04-08 15:38:38 -0500

    add caching

--- src/app.ts
+++ src/app.ts
@@ -14,9 +14,9 @@ const corsOptions = {
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-  standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  limit: 50, // Limit each IP to 50 requests per `window`
+  standardHeaders: 'draft-7', 
+  legacyHeaders: false, 
 });
 
 const router = express.Router();
--- src/services/locations.ts
+++ src/services/locations.ts
@@ -1,13 +1,22 @@
 import express, { Request, Response } from 'express';
 import { GetLocationsParams } from 'astro-ws-types';
 import { getLocationsByQuery } from './external/mapbox';
+import NodeCache from 'node-cache';
 
 const router = express.Router();
+const cache = new NodeCache({
+  stdTTL: 86400, // one day
+  checkperiod: 43200, // twice per day
+});
 
 router.get('/', async (req: Request<{}, {}, {}, GetLocationsParams>, res: Response) => {
   const { location } = req.query;
   if (!location) {
     return res.status(400).send({ message: '"location" is a required query parameter.' });
+  }
+  const cached = cache.get(location);
+  if (cached) {
+    return res.status(200).send(cached);
   }
   try {
     const queryResponse = await getLocationsByQuery(location);
@@ -15,6 +24,7 @@ router.get('/', async (req: Request<{}, {}, {}, GetLocationsParams>, res: Respon
       name: properties.place_formatted,
       coordinates: properties.coordinates,
     }));
+    cache.set(location, locations);
     return res.status(200).send(locations);
   } catch (e) {
     console.error(e);
